f18e9b02d71a130f89ca0865c4af4ce34157433b
Sardor <sardor-yuldashev-97@mail.ru>
2023-10-12 23:31:05 +0300

    2 day

--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -4,6 +4,22 @@
             <!-- /content -->
             <!-- side -->
             <div class="side">
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:menu",
+                    "left",
+                    Array(
+                        "ALLOW_MULTI_SELECT" => "N",
+                        "CHILD_MENU_TYPE" => "left",
+                        "DELAY" => "N",
+                        "MAX_LEVEL" => "1",
+                        "MENU_CACHE_GET_VARS" => array(""),
+                        "MENU_CACHE_TIME" => "3600",
+                        "MENU_CACHE_TYPE" => "N",
+                        "MENU_CACHE_USE_GROUPS" => "Y",
+                        "ROOT_MENU_TYPE" => "left",
+                        "USE_EXT" => "Y"
+                    )
+                );?>
                 <!-- side anonse -->
                 <div class="side-block side-anonse">
                     <div class="title-block"><span class="i i-title01"></span>Полезная информация!</div>
--- partneram/raspisanie-meropriyatiy/.section.php
+++ partneram/raspisanie-meropriyatiy/.section.php
@@ -1,6 +1,6 @@
 <?
 $sSectionName = "Расписание мероприятий";
 $arDirProperties = Array(
-
+   "menu_text" => "это текст для пункта \"Пункт 1\""
 );
 ?>
